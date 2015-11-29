bdffab400a5683f6d8776afa95af0a3ccf016d2a
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-11-29 17:36:53 +0100

    ee|iop: optimize compilation linking
    The purpose of linking is to replace DispatcherReg by an equivalent
    done at compile time.
    
    Previous code uses a binary search in the block manager to get the destination
    pointer of a given PC.
    
    The new code rely on the direct mapping which is already stored in recLUT
    
    v2: move & rename s_pCurBlock
    * no need to have a static for a local var
    * block function pointer must be correct to allow the new fast linking
    
    Extra Note: potentially a common function for iop/ee can be done

--- pcsx2/x86/iR3000A.cpp
+++ pcsx2/x86/iR3000A.cpp
@@ -64,7 +64,6 @@ u32 g_iopCyclePenalty;
 static EEINST* s_pInstCache = NULL;
 static u32 s_nInstCacheSize = 0;
 
-static BASEBLOCK* s_pCurBlock = NULL;
 static BASEBLOCKEX* s_pCurBlockEx = NULL;
 
 static u32 s_nEndBlock = 0; // what psxpc the current block ends
@@ -972,7 +971,7 @@ void psxSetBranchImm( u32 imm )
 	_psxFlushCall(FLUSH_EVERYTHING);
 	iPsxBranchTest(imm, imm <= psxpc);
 
-	recBlocks.Link(HWADDR(imm), xJcc32());
+	recBlocks.Link(HWADDR(imm), PSX_GETBLOCK(HWADDR(imm)), xJcc32());
 }
 
 static __fi u32 psxScaleBlockCycles()
@@ -1172,9 +1171,13 @@ static void __fastcall iopRecRecompile( const u32 startpc )
 	x86Align(16);
 	recPtr = x86Ptr;
 
-	s_pCurBlock = PSX_GETBLOCK(startpc);
+	{ // Update the function pointer of the current base block
+		BASEBLOCK* cur_base_block = PSX_GETBLOCK(startpc);
 
-	pxAssert(s_pCurBlock->GetFnptr() == (uptr)iopJITCompile);
+		pxAssert(cur_base_block->GetFnptr() == (uptr)iopJITCompile);
+
+		cur_base_block->SetFnptr((uptr)recPtr);
+	}
 
 	if (IsDevBuild) {
 		s_pCurBlockEx = recBlocks.Get(HWADDR(startpc));
@@ -1185,7 +1188,6 @@ static void __fastcall iopRecRecompile( const u32 startpc )
 
 	psxbranch = 0;
 
-	s_pCurBlock->SetFnptr( (uptr)x86Ptr );
 	s_psxBlockCycles = 0;
 
 	// reset recomp state variables
@@ -1344,7 +1346,7 @@ StartRecomp:
 			pxAssert( psxpc == s_nEndBlock );
 			_psxFlushCall(FLUSH_EVERYTHING);
 			MOV32ItoM((uptr)&psxRegs.pc, psxpc);
-			recBlocks.Link(HWADDR(s_nEndBlock), xJcc32() );
+			recBlocks.Link(HWADDR(s_nEndBlock), PSX_GETBLOCK(HWADDR(s_nEndBlock)), xJcc32() );
 			psxbranch = 3;
 		}
 	}
@@ -1358,7 +1360,6 @@ StartRecomp:
 
 	pxAssert( (g_psxHasConstReg&g_psxFlushedConstReg) == g_psxHasConstReg );
 
-	s_pCurBlock = NULL;
 	s_pCurBlockEx = NULL;
 }
 
--- pcsx2/x86/ix86-32/iR5900-32.cpp
+++ pcsx2/x86/ix86-32/iR5900-32.cpp
@@ -87,7 +87,6 @@ static u32 *recConstBufPtr = NULL;
 EEINST* s_pInstCache = NULL;
 static u32 s_nInstCacheSize = 0;
 
-static BASEBLOCK* s_pCurBlock = NULL;
 static BASEBLOCKEX* s_pCurBlockEx = NULL;
 u32 s_nEndBlock = 0; // what pc the current block ends
 u32 s_branchTo;
@@ -1123,7 +1122,7 @@ static void iBranchTest(u32 newpc)
 		if (newpc == 0xffffffff)
 			xJS( DispatcherReg );
 		else
-			recBlocks.Link(HWADDR(newpc), xJcc32(Jcc_Signed));
+			recBlocks.Link(HWADDR(newpc), PC_GETBLOCK(HWADDR(newpc)), xJcc32(Jcc_Signed));
 
 		xJMP( DispatcherEvent );
 	}
@@ -1703,9 +1702,14 @@ static void __fastcall recRecompile( const u32 startpc )
 		DbgCon.WriteLn("Compiling block @ 0x%08x", startpc);
 #endif
 
-	s_pCurBlock = PC_GETBLOCK(startpc);
+	{ // Update the function pointer of the current base block
+		BASEBLOCK* cur_base_block = PC_GETBLOCK(startpc);
 
-	pxAssert(s_pCurBlock->GetFnptr() == (uptr)JITCompile);
+		pxAssert(cur_base_block->GetFnptr() == (uptr)JITCompile);
+
+		cur_base_block->SetFnptr((uptr)recPtr);
+	}
+
 
 #ifdef PCSX2_DEBUG
 	s_pCurBlockEx = recBlocks.Get(HWADDR(startpc));
@@ -2071,7 +2075,6 @@ StartRecomp:
 
 	pxAssert( (pc-startpc)>>2 <= 0xffff );
 	s_pCurBlockEx->size = (pc-startpc)>>2;
-	s_pCurBlock->SetFnptr((uptr)recPtr);
 
 	if( !(pc&0x10000000) )
 		maxrecmem = std::max( (pc&~0xa0000000), maxrecmem );
@@ -2109,7 +2112,7 @@ StartRecomp:
 			{
 				xMOV( ptr32[&cpuRegs.pc], pc );
 				xADD( ptr32[&cpuRegs.cycle], eeScaleBlockCycles() );
-				recBlocks.Link( HWADDR(pc), xJcc32() );
+				recBlocks.Link( HWADDR(pc), PC_GETBLOCK(HWADDR(pc)), xJcc32() );
 			}
 		}
 	}
@@ -2125,7 +2128,6 @@ StartRecomp:
 
 	pxAssert( (g_cpuHasConstReg&g_cpuFlushedConstReg) == g_cpuHasConstReg );
 
-	s_pCurBlock = NULL;
 	s_pCurBlockEx = NULL;
 }
 
